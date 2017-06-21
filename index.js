ce1ed967f531dc814f346144d475febed79c65f0
Slava Knyazev <slava@knyz.org>
2017-06-21 17:09:15 -0400

    Added progressbar and did some improvements here and there

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var ProgressBar = require('progress');
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: false });
 
@@ -6,20 +7,33 @@ var fs = require("fs");
 /*** CONFIGURATION ***/
 
 //Link to the table of contents of the course you want
-var target = 
-"https://app.pluralsight.com/library/courses/java-fundamentals-language/table-of-contents";
+var target =
+    "https://app.pluralsight.com/library/courses/speaking-fundamentals/table-of-contents";
 
 //It will be saved to videos/folder-you-want
-var saveTo = "Java Fundamentals: The Java Language"
+//var saveTo = "D3: The Big Picture"
 
 //You need to login
 
 var user = {
-    email: "myname@email.com",
-    password: "Password123"
+    email: "john.doe@gmail.com",
+    password: "P45$W0RD123"
 }
 
 /*** DAH CODEZ ***/
+
+console.log(`
+Slava Knyazev:
+This program was written for EDUCATION and PERSONAL USE ONLY
+Please be respectful of the original authors' intellectual property
+
+This scraper is open source and licensed under GPLv2 on Github
+https://github.com/knyzorg/pluralsight-scraper
+`)
+
+console.log("Logging in...")
+
+var numberOfFiles, completed, saveTo, progress = 0;
 nightmare
     .goto('https://app.pluralsight.com/id/')
     .wait(1000)
@@ -37,35 +51,41 @@ nightmare
                 url: course.href
             })
         })
-        return courses.filter((thing) => thing.url);
-        //return document.querySelectorAll(".table-of-contents__title a")
+        return {
+            courses: courses.filter((thing) => thing.url),
+            title: document.title
+        }
     })
-    .then(function (courses) {
-        //console.log(JSON.stringify(courses));
-        var tasks = courses.map((course, index) => (
+    .then(function (module) {
+        numberOfFiles = module.courses.length;
+        saveTo = module.title.replace(" | Pluralsight", "");
+        console.log(`Downloading "${saveTo}" from PluralSight, ${numberOfFiles} videos`)
+        progress = new ProgressBar(':current/:total [:bar] :percent :etas', { total: numberOfFiles, callback: terminate })
+        var tasks = module.courses.map((course, index) => (
             (callback) => {
                 scrape(course, index, callback)
             }
         ))
         require("async.parallellimit")(tasks, 1, function () {
-});
+        });
     })
 
 function scrape(course, index, callback) {
     nightmare.goto(course.url)
         .wait("video")
-        .wait(1000)
+        .wait(1500)
         .evaluate(() => {
             var src = document.querySelector("video").src
             return src
-        }).then((result) => {
-            //console.log(result)
+        })
+        .then((result) => {
 
-            if (!result){
+            if (!result) {
+                progress.interrupt("Something went wrong. Retrying...")
                 scrape(...arguments)
                 return
             }
-            
+
             course.src = result
             saveVideo(course, index + 1)
             callback()
@@ -73,7 +93,7 @@ function scrape(course, index, callback) {
 }
 
 function saveVideo(course, number) {
-    console.log(number);
+    //console.log(number, course.name);
     if (!fs.existsSync("videos/")) {
         fs.mkdirSync("videos/");
     }
@@ -84,7 +104,18 @@ function saveVideo(course, number) {
         return;
     }
     var file = fs.createWriteStream("videos/" + saveTo + "/" + number + ". " + course.name.replace("/", "") + ".webm");
-    var request = http.get(course.src, function (response) {
+    var request = http.get(course.src,(response) => {
+        progress.tick()
         response.pipe(file);
+        completed++;
+        if (completed == numberOfFiles) {
+
+        }
     });
+}
+
+function terminate() {
+    console.log("Completed!")
+    console.log("Please keep these files for personal use to support the authors.")
+    process.exit(0)
 }
