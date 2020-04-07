c183d8e879b8bbf38bd4874bee77a59d49c78047
Slava Knyazev <slava@knyazev.io>
2020-04-06 23:09:15 -0400

    Complete rewrite to leverage pluralsight's API for scraping

--- index.js
+++ index.js
@@ -1,123 +1,105 @@
-require('dotenv').config()
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+//const cliProgress = require('cli-progress');
 
-// Link to the table of contents of the course you want
-var target = process.env.TARGET;
+const wait = ms => new Promise(r => setTimeout(r, ms));
+// const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-// Your login details
-var user = {
-    email: process.env.EMAIL,
-    password: process.env.PASSWORD
-}
+async function downloadCourse(courseId, cookie) {
+    async function getVideoUrl(clipId) {
+        let clipData = await fetch("https://app.pluralsight.com/video/clips/v3/viewclip", {
+            "headers": {
+                "Content-Type": "application/json",
+                cookie
+            },
+            "body": JSON.stringify({ "clipId": clipId, "mediaType": "mp4", "quality": "1280x720", "online": true, "boundedContext": "course", "versionId": "" }),
+            "method": "POST"
+        }).then((response) => response.json());
 
-/*** DAH CODEZ ***/
-var ProgressBar = require('progress');
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: false });
+        return clipData.urls[0].url;
+    }
 
-var https = require('https');
-var fs = require("fs");
+    const courseData = await fetch("https://app.pluralsight.com/learner/content/courses/" + courseId, {
+        headers: { cookie }
+    }).then((response) => response.json());
 
-var useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-
-console.log(`This program was written for EDUCATION and PERSONAL USE ONLY
-Please be respectful of the original authors' intellectual property
-
-This scraper is open source and licensed under GPLv2 on Github
-https://github.com/knyzorg/pluralsight-scraper
-`)
-
-console.log("Logging in...")
-
-var numberOfFiles, completed, saveTo, progress = 0;
-nightmare
-    .useragent(useragent)
-    .goto('https://app.pluralsight.com/id/')
-    .insert('#Username', user.email)
-    .insert('#Password', user.password)
-    .click('#login')
-    .wait(1000)
-    .goto(target)
-    .wait(3000)
-    .evaluate(function () {
-        var courses = [];
-        document.querySelectorAll('a[class^="clipListTitle"').forEach((course) => {
-            courses.push({
-                name: course.text,
-                url: course.href
-            })
-        })
-        return {
-            courses: courses.filter((thing) => thing.url),
-            title: document.title
-        }
-    })
-    .then(function (module) {
-        numberOfFiles = module.courses.length;
-        if (!numberOfFiles){
-            console.error("Wrong login credentials!")
-            process.exit(1)
-            return;
-        }
-        console.log("Logged in!")
-        saveTo = module.title.replace(" | Pluralsight", "");
-        console.log(`Downloading "${saveTo}" from PluralSight, ${numberOfFiles} videos`)
-        progress = new ProgressBar(':current/:total [:bar] :percent :etas', { total: numberOfFiles, callback: terminate })
-        var tasks = module.courses.map((course, index) => (
-            (callback) => {
-                scrape(course, index, callback)
-            }
-        ))
-        require("async.parallellimit")(tasks, 1, function () {
+    let videos = courseData.modules
+        // Get video items
+        .map(module => module.clips)
+        // Flatten from module structure
+        .reduce((a, b) => [...a, ...b], [])
+        // Reformat a bit, get rid of all the unncessary information, generate title
+        .map(async (video, index) => {
+            // Wait 200 seconds after each request to avoid throttling
+            await wait(10000 * index);
+            let url = await getVideoUrl(video.clipId);
+            
+            return {
+                // Add numbering for proper sequencing
+                // Strip title of illegal chars
+                title: `${index + 1}. ${video.title.replace(/[|&;$%@"<>()+,]/g, "")}`,
+                // Get video url
+                url
+            };
         });
-    }).catch((e) => console.log(e))
 
-function scrape(course, index, callback, delay=1500) {
-    nightmare
-        .useragent(useragent)
-        .goto(course.url)
-        .wait("video")
-        .wait(1500)
-        .evaluate(() => {
-            var src = document.querySelector("video").src
-            return src
-        })
-        .then((result) => {
-
-            if (!result) {
-                progress.interrupt("Something went wrong. Retrying...")
-                scrape(course, index, callback, delay+500)
-                return
-            }
-
-            course.src = result
-            saveVideo(course, index + 1)
-            callback()
-        }).catch((e) => console.log(e))
-}
-
-function saveVideo(course, number) {
-    //console.log(number, course.name);
-    if (!fs.existsSync("videos/")) {
-        fs.mkdirSync("videos/");
-    }
-    if (!fs.existsSync("videos/" + saveTo)) {
-        fs.mkdirSync("videos/" + saveTo);
-    }
-    if (fs.existsSync("videos/" + saveTo + "/" + number + ". " + course.name.replace(/[^a-z]/ig, "") + ".webm")) {
-        return;
-    }
-    var file = fs.createWriteStream("videos/" + saveTo + "/" + number + ". " + course.name.replace(/[^a-z]/ig, "") + ".webm");
-    var request = https.get(course.src,(response) => {
-        progress.tick()
-        response.pipe(file);
-        completed++;
-        if (completed == numberOfFiles) {
-
+    const videoFeed = {
+        [Symbol.asyncIterator]() {
+            return {
+                index: 0,
+                async next() {
+                    return { value: (await videos[this.index++]), done: this.index+1 == videos.length };
+                }
+            };
         }
-    });
+    };
+
+    const videoPath = path.join(__dirname, "videos");
+    const coursePath = path.join(videoPath, courseId.replace(/[|&;$%@"<>()+,]/g, ""));
+
+    if (!fs.existsSync(videoPath)) {
+        fs.mkdirSync(videoPath);
+    }
+    if (!fs.existsSync(coursePath)) {
+        fs.mkdirSync(coursePath);
+    }
+
+    for await (let video of videoFeed) {
+        const file = path.join(coursePath, video.title + ".mp4");
+
+        if (!fs.existsSync(file)) {
+            const data = await fetch(video.url);
+            const fileStream = fs.createWriteStream(file);
+            data.body.pipe(fileStream);
+        }
+    }
+
+
 }
 
-function terminate() {
-    console.log("Operation Completed!")
-    process.exit(0)
+// Retrieve login cookies
+async function login() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://app.pluralsight.com/id/');
+
+    while (page.url() != "https://app.pluralsight.com/library/") {
+        await page.waitForNavigation({ timeout: 0 });
+    }
+
+    const cookies = await page.cookies();
+
+    const cookieData = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join("; ");
+
+    fs.writeFileSync("./cookies.txt", cookieData);
+
+    await browser.close();
+};
+
+if (fs.existsSync("./cookies.txt")) {
+    downloadCourse("rust-fundamentals", fs.readFileSync("./cookies.txt").toString());
+} else {
+    login();
 }
